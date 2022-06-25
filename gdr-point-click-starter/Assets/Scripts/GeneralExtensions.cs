48e582f1d858276d2f7aba8e59359b1f50138312
yecats <yecats131@gmail.com>
2022-06-24 17:52:34 -0700

    new tutorial

---
+++ gdr-point-click-starter/Assets/Scripts/GeneralExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public static class GeneralExtensions
+{
+    public static Vector3 WithNewY(this Vector3 vector, float newY) => new Vector3(vector.x, newY, vector.z);
+}
